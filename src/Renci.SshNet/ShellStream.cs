d4db47b4b06d5ed57d356bbab0b8daeef846f4a6
adriacp05 <adriacp05@hotmail.es>
2024-01-05 09:51:32 +0100

    added windowchangerequest to shelltream method

--- src/Renci.SshNet/ShellStream.cs
+++ src/Renci.SshNet/ShellStream.cs
@@ -815,5 +815,22 @@ namespace Renci.SshNet
         {
             DataReceived?.Invoke(this, new ShellDataEventArgs(data));
         }
+
+        /// <summary>
+        /// Sends a "window-change" request.
+        /// </summary>
+        /// <param name="columns">The terminal width in columns.</param>
+        /// <param name="rows">The terminal width in rows.</param>
+        /// <param name="width">The terminal height in pixels.</param>
+        /// <param name="height">The terminal height in pixels.</param>
+        public void SendWindowChangeRequest(uint columns, uint rows, uint width, uint height)
+        {
+            if (_channel == null)
+            {
+                throw new ObjectDisposedException("ShellStream");
+            }
+
+            _ = _channel.SendWindowChangeRequest(columns, rows, width, height);
+        }
     }
 }
